54f0e964d79892a5d88009b3a3a2a4ff1bbc773b
Sagar Nirwal <62455271+Sagar247776@users.noreply.github.com>
2022-02-07 21:49:07 +0530

    L and N
    work on logo and navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,7 +10,7 @@ export class NavBar extends Component {
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
             <a className="navbar-brand" href="/">
-              Navbar
+              <img src="/logo.png" />
             </a>
             <button
               className="navbar-toggler"
@@ -35,46 +35,8 @@ export class NavBar extends Component {
                 </li>
                 <li className="nav-item">
                   <a className="nav-link" href="/">
-                    Link
+                    About
                   </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="/"
-                    id="navbarDropdown"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul
-                    className="dropdown-menu"
-                    aria-labelledby="navbarDropdown"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr className="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="/">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link disabled">Disabled</a>
                 </li>
               </ul>
               <form className="d-flex">
@@ -84,7 +46,7 @@ export class NavBar extends Component {
                   placeholder="Search"
                   aria-label="Search"
                 />
-                <button className="btn btn-outline-success" type="submit">
+                <button className="btn btn-outline-danger" type="submit">
                   Search
                 </button>
               </form>
